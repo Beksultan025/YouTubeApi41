872a672d0682b0dd67ce83d06db0f07fa23d89f0
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-06-09 04:29:10 +0600

    adapter added

--- app/src/main/java/kg/junesqo/youtubeapi41/ui/playlist/PlaylistActivity.kt
+++ app/src/main/java/kg/junesqo/youtubeapi41/ui/playlist/PlaylistActivity.kt
@@ -1,16 +1,16 @@
 package kg.junesqo.youtubeapi41.ui.playlist
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
 import android.view.LayoutInflater
+import android.widget.Adapter
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import kg.junesqo.youtubeapi41.R
 import kg.junesqo.youtubeapi41.base.BaseActivity
-import kg.junesqo.youtubeapi41.base.BaseViewModel
 import kg.junesqo.youtubeapi41.databinding.ActivityPlaylistBinding
+import kg.junesqo.youtubeapi41.model.Item
+import kg.junesqo.youtubeapi41.ui.playlist_detail.PlaylistDetailActivity
 
-class PlaylistActivity : BaseActivity<ActivityPlaylistBinding, PlaylistViewModel>() {
+class PlaylistActivity : BaseActivity<ActivityPlaylistBinding, PlaylistViewModel>(), Adapter.OnClick {
 
     override val viewModel: PlaylistViewModel by lazy {
         ViewModelProvider(this)[PlaylistViewModel::class.java]
@@ -20,22 +20,32 @@ class PlaylistActivity : BaseActivity<ActivityPlaylistBinding, PlaylistViewModel
         return ActivityPlaylistBinding.inflate(inflater)
     }
 
-    override fun initView() {
-
-    }
-
-    override fun initListener() {
-
+    override fun checkInternet() {
     }
 
     override fun initViewModel() {
         viewModel.getPlaylist().observe(this) {
             Toast.makeText(this, it.kind, Toast.LENGTH_SHORT).show()
+            initRecyclerView(it.items)
         }
     }
 
-    override fun checkInternet() {
+    override fun initView() {
 
+    }
+
+    override fun initListener() {
+    }
+
+    private fun initRecyclerView(playlistsList: List<Item>) {
+        binding.recyclerMain.adapter = PlaylistAdapter(playlistsList, object:PlaylistAdapter.OnItemClickListener{
+            override fun onItemClick(id: String) {
+                Intent(this@PlaylistActivity, PlaylistDetailActivity::class.java)
+                intent.putExtra("Username","John Doe")
+                startActivity(intent)
+            }
+
+        })
     }
 
 }
---
+++ app/src/main/java/kg/junesqo/youtubeapi41/ui/playlist_detail/PlaylistDetailActivity.kt
@@ -0,0 +1,12 @@
+package kg.junesqo.youtubeapi41.ui.playlist_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kg.junesqo.youtubeapi41.R
+
+class PlaylistDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist_detail)
+    }
+}
