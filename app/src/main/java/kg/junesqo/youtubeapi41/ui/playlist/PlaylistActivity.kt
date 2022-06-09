69c89db9e4782b5933164e5697f32f24a8dc0ffa
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-06-09 19:10:34 +0600

    check internet connection added

--- app/src/main/java/kg/junesqo/youtubeapi41/ui/playlist/PlaylistActivity.kt
+++ app/src/main/java/kg/junesqo/youtubeapi41/ui/playlist/PlaylistActivity.kt
@@ -1,8 +1,12 @@
 package kg.junesqo.youtubeapi41.ui.playlist
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Adapter
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
@@ -26,13 +30,15 @@ class PlaylistActivity : BaseActivity<ActivityPlaylistBinding, PlaylistViewModel
     }
 
     override fun checkInternet() {
+        checkConnection()
+        binding.networkLayout.btnTryAgain.setOnClickListener{
+            checkConnection()
+        }
+
     }
 
     override fun initViewModel() {
-        viewModel.getPlaylist().observe(this) {
-            Toast.makeText(this, it.kind, Toast.LENGTH_SHORT).show()
-            initRecyclerView(it.items)
-        }
+        initVM()
     }
 
     override fun initView() {
@@ -51,6 +57,44 @@ class PlaylistActivity : BaseActivity<ActivityPlaylistBinding, PlaylistViewModel
                 intent.putExtra(KEY,channelId)
                 startActivity(intent)
 
+    }
+
+    fun initVM() {
+        viewModel.getPlaylist().observe(this) {
+            Toast.makeText(this, it.kind, Toast.LENGTH_SHORT).show()
+            initRecyclerView(it.items)
+        }
+    }
+
+    private fun checkConnection() {
+        if (isOnline(this)){
+            binding.recyclerMain.visibility = View.VISIBLE
+            binding.networkLayout.root.visibility = View.GONE
+            initVM()
+        } else {
+            binding.recyclerMain.visibility = View.GONE
+            binding.networkLayout.root.visibility = View.VISIBLE
+        }
+    }
+
+    private fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+        return false
     }
 
 
